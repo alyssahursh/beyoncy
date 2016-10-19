6c49340e725b57d6c8e696c4b38b3bcc674d37e3
laurenfb <laurenfriesbrink@gmail.com>
2016-10-19 16:21:50 -0700

    added validations for address, order, review

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,11 @@
 class Address < ActiveRecord::Base
   belongs_to :user
+
+  # address must have a kind, last name, 1st line of street address, city, state, and country
+
+  validates_presence_of :kind, :last_name, :street1, :city, :state, :country
+
+  # address must be associated with a user. don't put this in the user validations or else we'll get an infinite loop!
+  validates :user_id, presence: true
+  validates_associated :user
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,14 @@
 class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :product
+
+  # must have a subject
+  validates :subject, presence: true
+
+  # review must be associated with a user & a product. don't put these in the user & product validations or else we'll get an infinite loop!
+  validates :user_id, presence: true
+  validates_associated :user
+
+  validates :product_id, presence: true
+  validates_associated :product
 end
