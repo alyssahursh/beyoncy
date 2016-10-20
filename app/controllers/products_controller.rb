99f295df9a4dac6d4ee2c9aee97c8780e865120c
Rachel <rachel.l.pavilanis@gmail.com>
2016-10-19 20:28:13 -0700

    added category dropdown to new product form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,18 +11,21 @@ class ProductsController < ApplicationController
 
   def new
     @product = Product.new
+    # @categories = Category.all.map{|c| [ c.name, c.id ] }
   end
 
   def create
     @product = Product.new(product_params)
+    @product.category_id = params[:category_id]
     if @product.save
-      redirect_to # UNKNOWN
+      redirect_to controller: 'pages', action: "index"
     else
-      render # UNKNOWN
+      render :action => :new
     end
   end
 
   def edit
+    @categories = Category.all.map{|c| [ c.name, c.id ] }
   end
 
   def update
@@ -48,7 +51,7 @@ class ProductsController < ApplicationController
   end
 
   def product_params
-    params.require(:product).permit(:name, :description, :price, :inventory_qty, :picture)
+    params.require(:product).permit(:name, :description, :price, :inventory_qty, :picture, :category_id)
   end
 
 end
