e6a9bdfe38d6855ee87d2bd69027d88564d2ad4b
jm-rives <joanna.rives@gmail.com>
2016-10-20 16:28:26 -0700

    FIXED udpate typo, added testing for omniAuth

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -5,10 +5,22 @@ class SessionsControllerTest < ActionController::TestCase
     request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:github]
   end
 
+  test 'user can log in successfully' do
+        OmniAuth.config.test_mode = true
+        OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({
+            'provider' => 'github',
+            'uid' => '12345678',
+            'info' => {'email' => 'tbecky@testmail.com', 'name' => 'test', 'image' => ''}
+        })
+        request.env['omniauth.env'] = OmniAuth.config.mock_auth[:facebook]
+
+        get :auth
+    end
+
   test 'can create a new user' do     
     assert_difference('User.count', 1) do
       login_a_user
-      assert_resonse :redirect
+      assert_response :redirect
       # may need to update this path in future
       asert_redirected_to root_path
     end
@@ -20,8 +32,49 @@ class SessionsControllerTest < ActionController::TestCase
 
     assert_no_difference('User.count') do
       login_a_user
-      assert_resonse :redirect
+      assert_response :redirect
       assert_redirected_to root_path
+    
     end
   end
+
+  # user data uniqueness 
+
+  test "user addresses should be unique" do
+      duplicate_adresses = @address.dup
+      @address.save
+      assert_not duplicate_address.valid?
+    end
+
+
+  test "index_addresses_on_user_id should be unique" do
+      duplicate_index_addresses_on_user_id = @index_addresses_on_user_id.dup
+      @index_addresses_on_user_id.save
+      assert_not duplicate_index_addresses_on_user_id.valid?
+    end
+
+  test 'user id (uid) should be unique' do
+    user_id = @uid.dup
+    @uid.save
+    assert_not duplicate_uid.valid?
+  end
+
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
