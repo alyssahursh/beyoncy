a1f47a807af30dbf9a60642d171adcd2e180b70c
jm-rives <joanna.rives@gmail.com>
2016-10-22 11:57:29 -0700

    Add attr_reader and one test

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -3,7 +3,18 @@ require 'test_helper'
 class PagesControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
+
+    # examples for future reference
+    # get :show, params: { id: 12 }
+    # patch update_url, params: { id: 12 }
+
     assert_response :success
+  end
+
+  test 'that the controller populates categories and products on get' do
+    get :index
+    assert(@controller.categories.length > 0)
+    assert(@controller.products.length > 0)
   end
 
 end
