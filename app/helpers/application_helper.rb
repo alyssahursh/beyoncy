07188b27553d8895dc426a3aee8f9d267f37af69
laurenfb <laurenfriesbrink@gmail.com>
2016-10-26 10:59:05 -0700

    buttons only show if user is admin, however second button needs some work

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
 
-  def active_for_button(user)
+  def active_for_button(object)
     # this looks counterintuitive, sorry! It's a view helper method to show the correct text on the button to mark a user active/inactive from the users index page for an admin.
-    if user.active
+    if object.active
       return "inactive"
     else
       return "active"
