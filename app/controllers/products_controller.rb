b47e2a6cb6f2c1707e320c015bd3b63828037f36
Rachel <rachel.l.pavilanis@gmail.com>
2016-10-25 21:41:35 -0700

    gem is working - can upload images for categories and products.  did a migration to remove :picture from tables and add :image column that is an attachment.  everything seems to have worked

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -71,7 +71,7 @@ class ProductsController < ApplicationController
   end
 
   def product_params
-    params.require(:product).permit(:name, :description, :price, :inventory_qty, :picture, :category_id)
+    params.require(:product).permit(:name, :description, :price, :inventory_qty, :category_id, :image)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161026035756) do
+ActiveRecord::Schema.define(version: 20161026043842) do
 
   create_table "addresses", force: :cascade do |t|
     t.string   "kind"
@@ -38,9 +38,12 @@ ActiveRecord::Schema.define(version: 20161026035756) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string   "picture"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "order_products", force: :cascade do |t|
@@ -69,7 +72,6 @@ ActiveRecord::Schema.define(version: 20161026035756) do
     t.string   "name"
     t.string   "description"
     t.integer  "price"
-    t.string   "picture"
     t.integer  "inventory_qty"
     t.boolean  "active"
     t.integer  "category_id"
