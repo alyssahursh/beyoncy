6896d3190e84338256eab8e18d027b4e2b642728
laurenfb <laurenfriesbrink@gmail.com>
2016-10-27 15:10:57 -0700

    tests for application helpers

---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+class ApplicationHelperTest < ActionView::TestCase
+
+  test "the truth" do
+    assert true
+  end
+
+  test 'if an active product object is passed to active_for_button it should return "inactive"' do
+    product = products(:normal_product)
+    result = active_for_button(product)
+    assert_equal(result, "inactive")
+  end
+
+  test 'if an inactive product object is passed to active_for_button it should return "active"' do
+    product = products(:inactive_product)
+    result = active_for_button(product)
+    assert_equal(result, "active")
+  end
+
+  test 'if an active user object is passed to active_for_button it should return "inactive"' do
+    user = users(:normal_user)
+    result = active_for_button(user)
+    assert_equal(result, "inactive")
+  end
+
+  test 'if an inactive user object is passed to active_for_button it should return "active"' do
+    user = users(:inactive_user)
+    result = active_for_button(user)
+    assert_equal(result, "active")
+  end
+end
